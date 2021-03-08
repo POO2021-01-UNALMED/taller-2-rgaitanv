71c3f94e28fc501e93d68b1ad818ad121d2b816f
rgaitanv <71475582+rgaitanv@users.noreply.github.com>
2021-03-08 00:13:28 -0500

    intento 1

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,42 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	Asiento asientos[] = new Asiento[10];
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados; 
+	
+	
+	
+	public int cantidadAsientos() {
+		
+		return asientos.length;
+		
+	}
+	
+	String verificarIntegridad() {
+		boolean asientosIguales = true;
+		
+		for(int i = 0; i < asientos.length-1; i++) {
+			if(asientos[0].registro != asientos[i+1].registro) {
+				asientosIguales = false;
+			}
+		}
+		if ( (motor.registro == registro) && asientosIguales) {
+				
+			return "Auto original";		
+			
+		}else {
+			
+			return "Las piezas no son originales";
+		}
+								
+	}
+	
+	
+	
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Motor {
+	
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+		
+	
+	void cambiarRegistro(int nuevoRegistro){
+		registro = nuevoRegistro;
+	}
+	
+	void asignarTipo(String nuevoTipo) {
+		
+		if(nuevoTipo == "electrico" || nuevoTipo== "gasolina") {
+			tipo = nuevoTipo;
+		}
+		
+	}
+}
